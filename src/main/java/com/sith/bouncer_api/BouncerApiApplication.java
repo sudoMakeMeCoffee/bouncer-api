dba6fd444998f74914646296d7e869c9c4d61ba9
sith <skdev.techno@gmail.com>
2025-07-27 16:48:33 +0530

    initialize project

---
+++ src/main/java/com/sith/bouncer_api/BouncerApiApplication.java
@@ -0,0 +1,13 @@
+package com.sith.bouncer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BouncerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BouncerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sith/bouncer_api/BouncerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sith.bouncer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BouncerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
