fc404b4b9c984cddbf57df8affeb9ca0d0447eab
sith <skdev.techno@gmail.com>
2025-09-07 14:56:40 +0530

    docs: add documentation

--- src/main/java/com/sith/api/controller/HomeController.java
+++ src/main/java/com/sith/api/controller/HomeController.java
@@ -1,19 +1,13 @@
 package com.sith.api.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class HomeController {
-    @GetMapping
-    public String hello(){
-        return "Bouncer Api";
-    }
 
-    @GetMapping("/docs")
-    public String docs(){
-        return "Docs";
+    @GetMapping("/")
+    public String home() {
+        return "index.html";
     }
 }
